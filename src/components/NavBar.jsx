2aca5629e6a70a80ebe2a949e53a3e25e561641e
kosturko123 <kosturko123@gmail.com>
2024-01-12 19:57:35 +0100

    first commit

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+function NavBar(){
+    return(
+        <div className='NavBar'>
+            <a>My Store</a>
+        </div>
+    );
+}
+
+export default NavBar
